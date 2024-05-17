6cc04dd13ef62ffb7997ea2eb8f23a2f88365c2b
theysamtheirs <sam@iamsamhankins.com>
2024-05-17 15:35:53 -0400

    initial commit, base chat setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Login from "$lib/components/Login.svelte";
+    import Messages from "$lib/components/Messages.svelte";
+    import { currentUser } from "$lib/pocketbase";
+  
+  
+  </script>
+  
+  <div class="container mx-auto my-12 ">
+      <h1>Pocketchat</h1>
+      
+      <Login />
+      
+      {#if $currentUser}
+      
+        <Messages />
+      
+      {/if}
+      
+  </div>
